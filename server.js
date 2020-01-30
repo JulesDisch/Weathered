d50ada984bb77d259b72d2d4ebf9c45d7c72332e
JulesDisch <53347157+JulesDisch@users.noreply.github.com>
2020-01-30 17:46:09 -0500

    heroku

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(session({
 // app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static('./weather-wear-040888'));
+  app.use(express.static("client/build"));
 }
 
 models.sequelize.sync().then(function () {
